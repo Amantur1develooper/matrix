bb1effebdcb80ae4a77563bfa03250b4dfd948c0
Амантур Эркинов <amanturerkinov@Noutbuk-Amantur.local>
2025-06-11 13:04:00 +0600

    m2

--- project/settings.py
+++ project/settings.py
@@ -121,14 +121,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Настройки корзины
-CART_SESSION_ID = 'cart'
+CART_SESSION_ID = 'cart' 
 
 # Настройки Telegram бота
 TELEGRAM_BOT_TOKEN = 'ваш-токен-бота'
